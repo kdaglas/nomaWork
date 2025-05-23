4848f01e56aad024001b5f828d731f310e3fc749
Douglas <daglach7@gmail.com>
2025-05-23 07:05:26 +0300

    Add the fixes

--- client/src/components/Review.tsx
+++ client/src/components/Review.tsx
@@ -1,25 +1,40 @@
+import { useState } from "react";
 import type { ReviewType } from "../types/ProductInterface";
+import { EditReview } from "./EditReview";
 type ReviewProp = {
+    id: string
     review: ReviewType;
 };
 
-export function Review({ review }: ReviewProp) {
+export function Review({ review, id }: ReviewProp) {
+
+    const [viewEdit, setViewEdit] = useState(false)
     return (
-        <div className="review">
-            <div className="d-flex justify-content-between">
-                <strong>{review.author}</strong>
-                <span>
-                    <i className="bi bi-star-fill"></i> {review.rating}
-                    
-                    <i className="bi bi-pencil-fill ms-4"></i>
-                    <i className="bi bi-trash-fill ms-2 text-danger"></i>
-                </span>
+        <>
+            <div className="review">
+                <div className="d-flex justify-content-between">
+                    <strong>{review.author}</strong>
+                    <span>
+                        <i className="bi bi-star-fill"></i> {review.rating}
+
+                        <button className="btn" onClick={() => setViewEdit(!viewEdit)}><i className="bi bi-pencil-fill ms-4"></i></button>
+                        <i className="bi bi-trash-fill ms-2 text-danger"></i>
+                    </span>
+                </div>
+                <p>{review.comment}</p>
+                <small className="text-muted">{review.date}</small>
             </div>
-            <p>{review.comment}</p>
-            <small className="text-muted">{review.date}</small>
-        </div>
 
+            {/* <button onClick={() => setViewEdit(!viewEdit)}>Edit Review</button> */}
+            {viewEdit &&
+                <EditReview
+                    id={id}
+                    reviewData={review}
+                    setViewEdit={() => setViewEdit(!viewEdit)}
+                />
+            }
 
+        </>
 
     )
 }
